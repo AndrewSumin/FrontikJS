c65241d0a1e413a0ca195d93a5007982d0efd33f
Andrew <AndrewSumin@gmail.com>
2011-08-05 03:09:40 +0400

    Hello world app

--- tests/app.js
+++ tests/app.js
@@ -1,3 +1,8 @@
 module.exports = {
-    apply: function(context){ console.log(context);}
+    apply: function(handler){
+        console.log(handler);
+        handler.response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        handler.response.write('<!DOCTYPE html><head><html><title>Hello world</title></head><body>Hello world</body></html>');
+        handler.response.end('\n');
+    }
 };
