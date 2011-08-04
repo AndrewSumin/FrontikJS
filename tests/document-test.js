1b0d48c2fed08e8c41b3c5fbcbb9fcdcbfe5b133
Andrew Sumin <AndrewSumin@gmail.com>
2011-08-04 22:01:04 +0000

    clear syntax

--- tests/document-test.js
+++ tests/document-test.js
@@ -1,17 +1,17 @@
 var vows = require('vows'),
     assert = require('assert'),
-    events = require('events')
-    document = require('../lib/document');
+    events = require('events'),
+    document = require('../lib/document'),
     defer = require('../lib/defer');
 
 vows.describe('Test doc').addBatch({
     'put static JSON': {
         topic: function(){
-            var promise = new(events.EventEmitter);
+            var promise = new (events.EventEmitter);
             var doc = document();
             doc.put("data1", {"foo":"bar"})
                .put("data2", {"foo":"bar"})
-               .then(function(res){promise.emit('success', res)})
+               .then(function(res){promise.emit('success', res);});
             return promise;
         },
 
@@ -22,16 +22,16 @@ vows.describe('Test doc').addBatch({
     },
     'put async JSON': {
         topic: function(){
-            var promise = new(events.EventEmitter);
+            var promise = new (events.EventEmitter);
             var doc = document();
             var defer1 = defer();
             var defer2 = defer();
             doc.put("data1", defer1)
                .put("data2", defer2)
-               .then(function(res){promise.emit('success', res)})
+               .then(function(res){promise.emit('success', res);});
 
-            setTimeout(function(){defer1.resolve({"foo":"bar"})}, 10);
-            setTimeout(function(){defer2.resolve({"foo":"bar"})}, 10);
+            setTimeout(function(){defer1.resolve({"foo":"bar"});}, 10);
+            setTimeout(function(){defer2.resolve({"foo":"bar"});}, 10);
             return promise;
         },
 
