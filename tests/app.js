21fd9e40fc124a1aa6cbcdf7617aae85377f1395
Andrew <AndrewSumin@gmail.com>
2011-08-09 02:45:30 +0400

    add debug mode

--- tests/app.js
+++ tests/app.js
@@ -4,7 +4,6 @@ module.exports = {
         handler.put('foo', handler.file(__dirname + '/json.js'));
         
         handler.then(function(doc){
-            console.log(handler.request.GET)
             handler.response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
             handler.response.write('<!DOCTYPE html><head><html><title>' + (handler.request.GET.name || doc.foo.message) + '</title></head><body>' + doc.foo.message + '</body></html>');
             handler.response.end('\n');
